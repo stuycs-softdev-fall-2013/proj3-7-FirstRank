487d14efb93a5affaa3ae0a932acd5186b8fcb71
Nancy Dai <babycarr6193@gmail.com>
2014-01-13 13:08:12 -0500

    playing around with navbar

---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template("index.html")
+
+@app.route('/About')
+def about():
+    return render_template("about.html")
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
