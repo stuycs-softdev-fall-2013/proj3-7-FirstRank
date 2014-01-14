9454470431e610e6d2979070b7f13d483138406f
Nancy Dai <babycarr6193@gmail.com>
2014-01-14 13:18:06 -0500

    playing with pages

--- app.py
+++ app.py
@@ -10,6 +10,24 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.route('/Events')
+def events():
+    d = []
+    a = {}
+    a['title'] = 'titleasddfkagjlkdsajfl;kadjsflk;adjskl;f'
+    a['info'] = 'ifskafjklsanfo'
+    b = {}
+    b['title'] = 'titlsadklfslakdjfklsadje'
+    b['info'] = 'infsakfhlsdao'
+    d.append(a)
+    d.append(b)
+    return render_template("events.html", events = d)
+
+#individual stat page
+@app.route('/Teams')
+def teams():
+    return render_template("teams.html")
+
 if __name__ == "__main__":
     app.debug = True
     app.run()
