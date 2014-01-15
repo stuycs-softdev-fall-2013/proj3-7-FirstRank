e4f7b2c833df142a4c85ee11a73f3e0b86c49ae7
Glib <gyd102@gmail.com>
2014-01-15 05:39:21 -0500

    front<->back functions

---
+++ db.py
@@ -0,0 +1,15 @@
+# will contain functions that pull info from api
+# and will convert the info to a form specifically
+# suited for the front end
+
+# full list of events and necessary info
+def getEvents():
+
+# full list of teams and necessary info
+# (including default calculations)
+def getTeams():
+
+# specifically generated stats based on
+# specified parameters
+paramters = {}
+def getGenStats(parameters):
