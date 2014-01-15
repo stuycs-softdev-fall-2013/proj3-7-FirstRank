887114921068ec769ab38e57210e3a6dc0c6f38f
Glib <gyd102@gmail.com>
2014-01-15 05:37:38 -0500

    middle branch, basics

--- app.py
+++ app.py
@@ -10,23 +10,33 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route('/Events')
-def events():
-    d = []
-    a = {}
-    a['title'] = 'titleasddfkagjlkdsajfl;kadjsflk;adjskl;f'
-    a['info'] = 'ifskafjklsanfo'
-    b = {}
-    b['title'] = 'titlsadklfslakdjfklsadje'
-    b['info'] = 'infsakfhlsdao'
-    d.append(a)
-    d.append(b)
-    return render_template("events.html", events = d)
+# returns an event page
+# a specific event_id will be reserved
+# to access a list of all events.
 
-#individual stat page
-@app.route('/Teams')
+default = "d0000"
+@app.route('/Events/<event_id>')
+def events():
+    events = []
+# events is a list of dictionaries. Each dictionary will store
+# all information about a specific event, including links to
+# specific teams
+    if event_id == default:
+# return master-list of all events. table format(?)
+        return render_template("events.html", events = events)
+# returns specific event page
+    return render_template("spec_event.html",
+                           event = events[event_id]);
+
+# works the same way as the Events page
+# same default value
+@app.route('/Teams/<team_id>')
 def teams():
-    return render_template("teams.html")
+    teams = []
+    if team_id == default:
+        return render_template("teams.html", teams = teams)
+    return render_template("spec_team.html",
+                           team = teams[team_id])
 
 if __name__ == "__main__":
     app.debug = True
