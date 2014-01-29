34a7398a46a4708002fdaf6a80d8bf623a893b5d
Nancy Dai <babycarr6193@gmail.com>
2014-01-29 04:23:46 -0500

    fixing up function names

--- app.py
+++ app.py
@@ -5,18 +5,16 @@ import db
 
 app = Flask(__name__)
 
-year = time.localtime()[0] -1
+year = time.localtime()[0]
+month = time.localtime()[1]
+day = time.localtime()[2]
 
 @app.route('/')
 def home():
-    return render_template("index.html")
-
-#@app.route('/Search', methods=['GET', 'POST'])
-#def search():
- #   if request.method == "GET":
-  #      return render_template("search.html")
-   # else:
-    #    return render_template("search.html") 
+    stats = []
+    if month > 2 or (month == 2 and day == 28):
+        stats = db.get_current_stats(year)
+    return render_template("index.html", teams=stats)
 
 @app.route('/<link>')
 def about(link):
@@ -30,22 +28,24 @@ def events(event_id):
         return render_template("events.html", events = events)
     else:
         events = api.event_info(event_id)
-        teams = db.get_event(event_id)
+        teams = db.get_event_stats(event_id)
         return render_template("spec_event.html", event = events, teams=teams)
 
 @app.route('/Teamlist/<page_num>')
 def teamlist(page_num=1):
     page = int(page_num)
-#    teams = db.team_compiler(page)
-    teams = db.get_year_stats(year)
+    if month < 2:
+        year = year - 1
+    teams = db.get_teamlist(year, page)
     d = {'page':page, 'total':54}
     return render_template("team.html", teams = teams, d=d) 
 
 @app.route('/Teams/<team_id>')
 def teams(team_id):
     teams = api.team_info(team_id)
-    stat = {}
-    return render_template("spec_team.html", team = teams, stats=stat)
+    stats = db.get_team_overall_stats(team_id)
+    event_stats = db.get_team_event_stats(team_id)
+    return render_template("spec_team.html", team = teams, stats=stats, events=event_stats)
 
 if __name__ == "__main__":
     app.debug = True
